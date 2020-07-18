c2400ab09ac0dc7e76e6991d48c461983da1e8b3
hamzataougar <taougarhamza69@gmail.com>
2020-07-18 03:21:38 +0100

    First commit of the project

---
+++ app.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+  text.innerText = 'Breathe In!';
+  container.className = 'container grow';
+
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
+setInterval(breathAnimation, totalTime);
+
+//Player Volume 
+audioPlayer = document.querySelector('#player');
+audioPlayer.volume = 0.1;
